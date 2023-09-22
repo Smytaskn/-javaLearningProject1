bfa77a3e14ce191c16428b5a226e39bd1e8b9f1f
Smytaskn <122823234+Smytaskn@users.noreply.github.com>
2023-09-22 21:36:11 +0300

    25 to 28

---
+++ E26/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+Book.Chapter chapter1=new Book().new Chapter();
+
+Book book=new Book();
+Book.Chapter chapter2=book.new Chapter();
+
+        Chapter c=new Book.Chapter();
+        Chapter c=new Chapter();
+
+        System.out.println("Hello world!");
+         */
+
+        Book.Publisher publisher=new Book.Publisher();
+
+        publisher.name="Fred";
+
+        book1.setPublisher(publisher);
+        () -> book1
+
+
+
+    }
+}
